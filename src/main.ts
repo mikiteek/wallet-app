126b2df5e04cbb353653e7a9e060399ce21195ab
Eric Cartman <58071469+mikiteek@users.noreply.github.com>
2025-11-26 13:18:57 +0100

    WA-13 Add initial config module (#1)
    version: minor
    changelog:
     - install nest/config
     - add config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('APP_PORT', 3000);
+
+  await app.listen(port, () => {
+    console.log(`Server is running on port ${port} !!!!!!!!!!!!`);
+  });
 }
-bootstrap();
+
+bootstrap()
+  .then(() => console.log('Server started successfully.'))
+  .catch((err) => {
+    console.log('Server up failed, exiting process', err);
+    process.exit(1);
+  });
