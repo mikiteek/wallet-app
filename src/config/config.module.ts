32250892451af70e85f02753eda8c18cd9afeca2
Eric Cartman <58071469+mikiteek@users.noreply.github.com>
2025-11-26 15:05:43 +0100

    WA-1.2 Load logger config async (#3)
    changelog:
     - load logger config async

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
 import serverConfig from './server/server.config.namespace';
+import loggerConfig from './logger/logger.config.namespace';
 
 @Module({
   imports: [
     NestConfigModule.forRoot({
-      load: [serverConfig],
+      load: [serverConfig, loggerConfig],
       isGlobal: true,
     }),
   ],
--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -1,17 +1,30 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerConfig } from '../config/logger/logger.config';
 
 @Module({
   imports: [
-    PinoLoggerModule.forRoot({
-      pinoHttp: {
-        level: 'debug',
-        transport: {
-          target: 'pino-pretty',
-          options: {
-            colorize: true,
+    PinoLoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const loggerConfig = configService.get<LoggerConfig>('logger');
+        if (!loggerConfig) {
+          throw new Error('Missing logger config');
+        }
+
+        return {
+          pinoHttp: {
+            level: loggerConfig.level,
+            transport: {
+              target: 'pino-pretty',
+              options: {
+                colorize: true,
+              },
+            },
           },
-        },
+        };
       },
     }),
   ],
